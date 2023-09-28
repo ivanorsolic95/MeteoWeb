6a1b4d69e2dad64dd7adb0e2c484b313682dd6bc
Ivan Orsolic <orsolic1995@gmail.com>
2023-09-28 23:33:52 +0200

    Add MapLocation component
    Utilize vue-leaflet template to create MapLocation component for displaying my location on a map.

---
+++ meteoweb/src/components/MapLocation.vue
@@ -0,0 +1,32 @@
+<template>
+    <div style="height:300px; width:600px">
+      <l-map ref="map" :zoom="zoom" :center="center">
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        ></l-tile-layer>
+        <l-marker :lat-lng="center"></l-marker>
+      </l-map>
+    </div>
+  </template>
+  
+  <script>
+  import "leaflet/dist/leaflet.css";
+  import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+  
+  export default {
+    components: {
+      LMap,
+      LTileLayer,
+      LMarker,
+    },
+    data() {
+      return {
+        zoom: 13,
+        center: [45.05410, 18.64830],
+      };
+    },
+  };
+  </script>
+  
+  <style></style>
+  
