4d0cb16efd7429577f46de364e5be91232718244
Ivan Orsolic <orsolic1995@gmail.com>
2023-09-28 23:31:19 +0200

    Update App component
    Import and render MapLocation component in App component.

--- meteoweb/src/App.vue
+++ meteoweb/src/App.vue
@@ -1,27 +1,25 @@
 <template>
   <div>
     <LineChart/>
+    <MapLocation/>
 </div>
 </template>
 
 <script>
 import LineChart from './components/LineChart.vue'
+import MapLocation from './components/MapLocation.vue';
 
 export default {
   name: 'App',
   components: {
-    LineChart
+    LineChart,
+    MapLocation
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
