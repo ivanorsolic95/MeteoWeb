6062b83785ff983abe49afaa56a38edc524eaa0f
Ivan Orsolic <orsolic1995@gmail.com>
2024-04-09 16:04:17 +0200

    Add the main header to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
 import NavBar from "../components/NavBar"
 
+const mainHeader = "Build your own station!"
+
 const IndexPage = () => {
   return (
     <body>
       <NavBar></NavBar>
+      <div id="body">
+        <h1>{mainHeader}</h1>
+      </div>
     </body>
   )
 }
