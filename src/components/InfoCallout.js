8e7a0cd967126c96ec8106b28957fa48958e2d02
Ivan Orsolic <orsolic1995@gmail.com>
2024-04-11 17:16:52 +0200

    Create InfoCallout component

--- src/components/InfoCallout.js
+++ src/components/InfoCallout.js
@@ -1,16 +1,28 @@
 import React from "react";
-import "../styles/info-callout.css"
-import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import "../styles/info-callout.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import DOMPurify from 'dompurify';
 
-const InfoCallout = ({infoText}) => {
+const InfoCallout = ({ infoText }) => {
+    
+    const hasHtmlTags = /<\/?[a-z][\s\S]*>/i.test(infoText);
+
+    const createMarkup = (htmlString) => {
+        return { __html: DOMPurify.sanitize(htmlString) };
+    };
+
     return (
         <div className="info-callout">
             <div className="info-notice">
-                <FontAwesomeIcon icon={faCircleInfo}/>
+                <FontAwesomeIcon icon={faCircleInfo} />
                 <p className="notice-text">Info</p>
-            </div> 
-            <span className="info-text">{infoText}</span>
+            </div>
+            {hasHtmlTags ? (
+                <span className="info-text" dangerouslySetInnerHTML={createMarkup(infoText)} />
+            ) : (
+                <span className="info-text">{infoText}</span>
+            )}
         </div>
     );
 };
