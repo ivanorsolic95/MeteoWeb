1dd705aba693ae100fde5da986aacc473cba8f86
Ivan Orsolic <orsolic1995@gmail.com>
2024-04-11 17:16:35 +0200

    Create AlertCallout component

---
+++ src/components/AlertCallout.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/alert-callout.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import DOMPurify from 'dompurify';
+
+const AlertCallout = ({ alertText }) => {
+    
+    const hasHtmlTags = /<\/?[a-z][\s\S]*>/i.test(alertText);
+
+    const createMarkup = (htmlString) => {
+        return { __html: DOMPurify.sanitize(htmlString) };
+    };
+
+    return (
+        <div className="alert-callout">
+            <div className="alert-notice">
+                <FontAwesomeIcon icon={faTriangleExclamation} />
+                <p className="alert-text">Alert</p>
+            </div>
+            {hasHtmlTags ? (
+                <span className="alert-callout-text" dangerouslySetInnerHTML={createMarkup(alertText)} />
+            ) : (
+                <span className="alert-callout-text">{alertText}</span>
+            )}
+        </div>
+    );
+};
+
+export default AlertCallout;
