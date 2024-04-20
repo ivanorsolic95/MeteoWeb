0e11a4d62257032bf205f84849e940f4dcd2021c
Ivan Orsolic <orsolic1995@gmail.com>
2024-04-20 14:17:06 +0200

    Revise the code snippets to use "react-syntax-highlighter"
    Use "react-syntax-highlighter" instead of the HTML "code" element, to improve readability of the code snippets.

--- src/pages/oled.js
+++ src/pages/oled.js
@@ -5,8 +5,20 @@ import OledLib from '../images/oledlib.png'
 import CheckOledLib from '../images/checkoledlib.png'
 import TipCallout from '../components/TipCallout'
 import TestDisplay from '../images/testdisplay.png'
+import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
+import { white} from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const Oled = () => {
+    const codeBlock = `from machine import Pin, SoftI2C 
+import ssd1306 
+i2c = SoftI2C(scl=Pin(15), sda=Pin(4))
+rst = Pin(16, Pin.OUT) 
+rst.value(1) 
+oled = ssd1306.SSD1306_I2C(128, 64, i2c) 
+oled.fill(0) 
+oled.text('Hello World', 0, 0)
+oled.show()`;
+
     return (
         <body>
             <NavBar/>
@@ -17,29 +29,23 @@ const Oled = () => {
                 </p>
                 <h2>OLED display library</h2>
                 <p className="text">
-                    The first step is to download <a href="https://github.com/adafruit/micropython-adafruit-ssd1306">the library</a> for the display, and save it to a location on your computer that you are familiar with. To upload the library onto the microcontroller,  open the terminal and run <code>&lt;ampy --port COMX --baud 115200 put x:\location\ssd1306.py&gt;</code>, where “X” should be switched with the port number, and “x:\location\” with the actual location of the ssd1306.py file.
+                    The first step is to download <a href="https://github.com/adafruit/micropython-adafruit-ssd1306">the library</a> for the display, and save it to a location on your computer that you are familiar with. To upload the library onto the microcontroller,  open the terminal and run <code><b><i>ampy --port COMX --baud 115200 put x:\location\ssd1306.py</i></b></code>, where “X” should be switched with the port number, and “x:\location\” with the actual location of the ssd1306.py file.
                 </p>
                 <AlertCallout alertText={'Before uploading the library via ampy in the terminal, ensure to close the session in MobaXTerm, as only one program can use the connection to the microcontroller at a time.'}/>
                 <img src={OledLib} alt="Oled library"></img>
                 <p class="text">
-                    To check if the library is successfully uploaded, connect to the microcontroller via MobaXTerm. In the command prompt, run <code>&lt;import os&gt;</code>, than <code>&lt;os.listdir()&gt;</code>. You should see “['boot.py', 'ssd1306.py']˙ ” in the next line.
+                    To check if the library is successfully uploaded, connect to the microcontroller via MobaXTerm. In the command prompt, run <code><b><i>import os</i></b></code>, than <code><b><i>os.listdir()</i></b></code>. You should see <b>“['boot.py', 'ssd1306.py']˙ ”</b> in the next line.
                 </p>
                 <img src={CheckOledLib} alt="Oled library check"></img>
                 <h2>Showing text on display </h2>
-                <pre><code>
-                    from machine import Pin, SoftI2C 
-                    import ssd1306 
-                    i2c = SoftI2C(scl=Pin(15), sda=Pin(4))
-                    rst = Pin(16, Pin.OUT) 
-                    rst.value(1) 
-                    oled = ssd1306.SSD1306_I2C(128, 64, i2c) 
-                    oled.fill(0) 
-                    oled.text('Hello World', 0, 0)
-                    oled.show() 
-                </code></pre>
+
+                <SyntaxHighlighter language="python" style={white} showLineNumbers>
+                    {codeBlock}
+                </SyntaxHighlighter>
+
                 <TipCallout tipText={'Notice in the third line that the SCL and SDA pins of my microcontroller are 15 and 4.  If you have a different microcontroller, it’s possible that the pins are not the same. Check your microcontroller’s documentation to see its SCL and SDA pins.'}/>
                 <img src={TestDisplay} alt="Testing display"/>
-                <p class="text">Take a look at your microcontroller’s display. You should see “Hello World” displayed on it.</p> 
+                <p class="text">Take a look at your microcontroller’s display. You should see <b>“Hello World”</b> displayed on it.</p> 
             </div>
         </body>
     );
