fed659563f189a26900b5aac4eb20d897d109924
Ivan Orsolic <orsolic1995@gmail.com>
2024-04-22 18:06:12 +0200

    Create Demo component that represents the webpage with the route /demo

---
+++ src/pages/demo.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import NavBar from '../components/NavBar';
+import mediumZoom from 'medium-zoom';
+import MeteoStation from '../images/Demo.jpg'
+
+const Demo = () => {
+    useEffect(() => {
+        const zoom = mediumZoom('[data-zoomable]', { margin: 0, scrollOffset: 0 });
+        return () => zoom.detach();
+      }, []);
+
+    return (
+        <body>
+            <NavBar/>
+            <div id='body'>
+                <h1 id="main-heading">You can see my weather station in action!</h1>
+                <img 
+                    loading="lazy" 
+                    alt="A weather station in action" 
+                    src={MeteoStation} 
+                    data-zoomable 
+                    style={{width:'80%',
+                            cursor: 'zoom-in'
+                     }} 
+                />
+            </div>
+        </body>
+    );
+};
+
+export default Demo;
+
+export const Head = () => <title>Demo</title>
