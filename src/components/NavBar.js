c68d8630c7c29754a058ac0b8761bad383166fc3
Ivan Orsolic <orsolic1995@gmail.com>
2024-04-22 18:06:45 +0200

    Add the link which points to the webpage with the route /demo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,7 +51,7 @@ const NavBar = () => {
                           <Link to='/code/' className="nav-link">Code</Link>
                       </li>
                       <li className="nav-item">
-                          <a className="nav-link" href="#">Demo</a>
+                          <Link to='/demo/' className="nav-link">Demo</Link>
                       </li>
                   </ul>
              </div>
@@ -96,7 +96,7 @@ const NavBar = () => {
                         <Link to='/code/' className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Code</Link>
                         </li>
                         <li>
-                        <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Demo</Link>
+                        <Link to='/demo/' className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Demo</Link>
                         </li>
 
                     </ul>
