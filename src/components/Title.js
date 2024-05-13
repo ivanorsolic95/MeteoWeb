a8b0d432a20b05631ec2f1a3f9c0fd64acae8237
Ivan Orsolic <orsolic1995@gmail.com>
2024-05-13 16:59:10 +0200

    Create Title layout component

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const Title = () => {
+    return (
+        <title>
+            MeteoWeb: Tutorial on how to build your own weather station!
+        </title>
+    )
+}
+
+export default Title;
